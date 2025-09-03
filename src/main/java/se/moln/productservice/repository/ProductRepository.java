ad2b913e1a565b144f6ef23218afa0aa2f052244
Ali Rezai <ali.rezai27461@gmail.com>
2025-09-03 11:09:40 +0200

    delete a rad code

--- src/main/java/se/moln/productservice/repository/ProductRepository.java
+++ src/main/java/se/moln/productservice/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package se.moln.productservice.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import se.moln.productservice.model.Product;
