cb4b72a0ccc99e0519c7d799b30f705beb6f7b8b
david-andreasson <79davand@gafe.molndal.se>
2025-09-18 10:15:59 +0200

    updated corsconfig to allow new frontend url

--- src/main/java/se/moln/productservice/config/CorsConfig.java
+++ src/main/java/se/moln/productservice/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3000", "https://productservice.drillbi.se/", "https://userservice.drillbi.se")
+                .allowedOrigins("http://localhost:3000", "https://productservice.drillbi.se/", "https://userservice.drillbi.se", "https://kebabrolle.drillbi.se/", "https://orderservice.drillbi.se/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
